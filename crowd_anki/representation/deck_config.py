76f60af2d638859544f9ed6c351af598743d5034
Vladyslav Sitalo <root@stvad.org>
2016-07-03 18:41:12 +0200

    deck config update if exists

--- crowd_anki/representation/deck_config.py
+++ crowd_anki/representation/deck_config.py
@@ -17,8 +17,15 @@ class DeckConfig(JsonSerializableAnkiDict):
         # Todo whole uuid matching thingy
         # For now only create scenario
 
-        new_id = collection.decks.confId(self.anki_dict["name"], self.anki_dict)
-        self.anki_dict = collection.decks.getConf(new_id)
+        config_dict = collection.decks.get_deck_config_by_uuid(self.get_uuid())
+
+        if config_dict:
+            config_dict.update(self.anki_dict)
+        else:
+            new_id = collection.decks.confId(self.anki_dict["name"], self.anki_dict)
+            config_dict = collection.decks.getConf(new_id)
+
+        self.anki_dict = config_dict
 
         collection.decks.save()
         collection.decks.flush()
