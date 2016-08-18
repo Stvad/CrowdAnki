57183ffdae407e39c0b078811fbf293bafa30f52
Vladyslav Sitalo <root@stvad.org>
2016-08-19 00:04:57 +0200

    more cleanup

--- crowd_anki/anki_overrides/decks.py
+++ crowd_anki/anki_overrides/decks.py
@@ -6,7 +6,8 @@ def get_card_ids(self, did, children=False, include_from_dynamic=False):
     deck_ids = [did] + ([deck_id for _, deck_id in self.children(did)] if children else [])
 
     request = "select id from cards where did in {}" + ("or odid in {}" if include_from_dynamic else "")
-    parameters = (anki.utils.ids2str(deck_ids),) + ((anki.utils.ids2str(deck_ids),) if include_from_dynamic else tuple())
+    parameters = (anki.utils.ids2str(deck_ids),) + ((anki.utils.ids2str(deck_ids),)
+                                                    if include_from_dynamic else tuple())
 
     return self.col.db.list(request.format(*parameters))
 
