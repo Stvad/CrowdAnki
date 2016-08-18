5c6775cd80d5d9a5971b4a72120f8dce019f5bcf
Vladyslav Sitalo <root@stvad.org>
2016-08-19 00:00:40 +0200

    main cleanup

--- crowd_anki/main.py
+++ crowd_anki/main.py
@@ -1,33 +1,16 @@
-import os.path
 from pathlib import Path
 
 from crowd_anki import anki_exporter_wrapper  # To hook exporters list extension
 from crowd_anki.anki_importer import AnkiJsonImporter
-from crowd_anki.anki_exporter import AnkiJsonExporter
 
 import aqt.utils
-from anki import Collection
 from aqt import mw, QAction, QFileDialog
-
-COLLECTION_PATH = "../../WCollection/collection.anki2"
 
 
 def main():
-    deck_name = "tdeckl1"
-    collection = Collection(COLLECTION_PATH)
-    print(os.path.realpath(os.path.curdir))
-
-    exporter = AnkiJsonExporter(collection)
-    exporter.export_deck_to_directory(deck_name)
-
-    # deck_directory = os.path.join("./", deck_name)
-    deck_directory = Path(deck_name)
-    # deck_json = os.path.join(deck_directory, deck_name + ".json")
-
-    # importer = AnkiJsonImporter(collection)
-    # importer.load_from_directory(deck_directory)
-
-    collection.close()
+    """
+    Todo: Implement command line interface
+    """
 
 
 def on_import_action():
