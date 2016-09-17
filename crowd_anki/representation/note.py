dd924004386a0ad4028f85f528d80ed67c1d2b4a
Vladyslav Sitalo <root@stvad.org>
2016-09-17 12:37:39 +0200

    optional ui import

--- crowd_anki/representation/note.py
+++ crowd_anki/representation/note.py
@@ -2,7 +2,12 @@ import anki
 import anki.utils
 from anki.notes import Note as AnkiNote
 
-from crowd_anki.anki_overrides.change_model_dialog import ChangeModelDialog
+try:
+    from crowd_anki.anki_overrides.change_model_dialog import ChangeModelDialog
+except ImportError:
+    # need this to decouple it from UI part. To simplify test running
+    print("Failed to import ChangeModelDialog")
+    
 from crowd_anki.utils.constants import UUID_FIELD_NAME
 from .json_serializable import JsonSerializableAnkiObject
 from .note_model import NoteModel
