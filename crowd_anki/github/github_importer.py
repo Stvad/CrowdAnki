5ef28425efe0364e3ac1bec71d0b4b94b363efe6
Merlin Kramer <namibj.der.echte@gmail.com>
2016-09-28 00:32:26 +0200

    Fixed PR issues on Stvad/CrowdAnki#13 as per code review.

--- crowd_anki/github/github_importer.py
+++ crowd_anki/github/github_importer.py
@@ -34,36 +34,38 @@ class GithubImporter(object):
                                         'Path:', text='<name>/<repository>')
         if repo and ok:
             self.download_and_import(repo)
+    def download_and_import_git(self, github_repo, deck_base_name):
+        repo = None
+        try:
+            repo = Repo(Path(self.collection.media.dir()).joinpath("..", "CrowdAnkiGit", deck_base_name, "git"))
+            repo.remote("origin").pull()
+        except git.exc.InvalidGitRepositoryError: # New repository
+            repo = Repo.clone_from(GITHUB_REPO.format(github_repo), Path(self.collection.media.dir()).joinpath("..", "CrowdAnkiGit", deck_base_name, "git"))
+        AnkiJsonImporter.import_deck(self.collection, Path(repo.working_tree_dir))
 
+    def download_and_import_zip(self, github_repo, deck_base_name):
+        response = urllib2.urlopen(GITHUB_LINK.format(github_repo))
+        response_sio = StringIO.StringIO(response.read())
+        with zipfile.ZipFile(response_sio) as repo_zip:
+            repo_zip.extractall(tempfile.tempdir)
+
+        deck_directory_wb = Path(tempfile.tempdir).joinpath(deck_base_name + "-" + BRANCH_NAME)
+        deck_directory = Path(tempfile.tempdir).joinpath(deck_base_name)
+        utils.fs_remove(deck_directory)
+        deck_directory_wb.rename(deck_directory)
+        # Todo progressbar on download
+        
+        AnkiJsonImporter.import_deck(self.collection, deck_directory)
     def download_and_import(self, github_repo):
         deck_base_name = github_repo.split("/")[-1]
         try:
             try:
-                repo = None
-                try:
-                    repo = Repo(Path(self.collection.media.dir()).joinpath("..", "CrowdAnkiGit", deck_base_name, "git"))
-                    repo.remote("origin").pull()
-                except git.exc.InvalidGitRepositoryError: # New repository
-                    repo = Repo.clone_from(GITHUB_REPO.format(github_repo), Path(self.collection.media.dir()).joinpath("..", "CrowdAnkiGit", deck_base_name, "git"))
-                AnkiJsonImporter.import_deck(self.collection, Path(repo.working_tree_dir))
-            
+                self.download_and_import_git(github_repo, deck_base_name
             except git.exc.GitCommandNotFound: # Git not avaiable. Use .zip archive instead
-                QErrorMessage.qtHandler().showMessage('Error accessing the git executable.\n'
-                                                  'Please make sure git is installed and avaiable on your PATH.\n'
-                                                  'Downgrading to zip archive download.')
-                response = urllib2.urlopen(GITHUB_LINK.format(github_repo))
-                response_sio = StringIO.StringIO(response.read())
-                with zipfile.ZipFile(response_sio) as repo_zip:
-                    repo_zip.extractall(tempfile.tempdir)
-    
-                
-                deck_directory_wb = Path(tempfile.tempdir).joinpath(deck_base_name + "-" + BRANCH_NAME)
-                deck_directory = Path(tempfile.tempdir).joinpath(deck_base_name)
-                utils.fs_remove(deck_directory)
-                deck_directory_wb.rename(deck_directory)
-                # Todo progressbar on download
-
-                AnkiJsonImporter.import_deck(self.collection, deck_directory)
-
+                print 'Error accessing the git executable.\n'\
+                        'Please make sure git is installed and avaiable on your PATH.\n'\
+                        'Downgrading to zip archive download.'
+                self.download_and_import_zip(github_repo, deck_base_name)
+                                             
         except (urllib2.URLError, urllib2.HTTPError, OSError) as error:
             aqt.utils.showWarning("Error while trying to get deck from Github: {}".format(error))
