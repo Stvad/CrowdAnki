34739992550550902681d27522077b71bdca62ee
Vladyslav Sitalo <sitalov@amazon.com>
2018-04-07 13:49:22 +0100

    Consolidate __init__.py files.

--- __init__.py
+++
@@ -1,4 +0,0 @@
-try:
-    from . import crowd_anki_importer
-except ValueError:
-    pass
--- crowd_anki/__init__.py
+++ crowd_anki/__init__.py
@@ -1,6 +1,9 @@
 try:
     # Register for https://github.com/dayjaby/AnkiHub plugin system.
     import ankihub
+
     ankihub.update(['Stvad/CrowdAnki'])
 except:
     pass
+
+from . import main
