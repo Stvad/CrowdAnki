268d834e824090a3ad02bf434a72b73767ca32c7
Vladyslav Sitalo <sitalov@amazon.com>
2019-04-07 20:40:45 +0100

    Sanitize `\n` from the deck directory name #44 (windows specific)

--- test/utils/filesystem/name_sanitizer_spec.py
+++ test/utils/filesystem/name_sanitizer_spec.py
@@ -1,12 +1,13 @@
 from tempfile import TemporaryDirectory
 
 from expects import expect
-from hypothesis import given, assume
+from hypothesis import given, assume, example
 from hypothesis.strategies import text, characters
 from mamba import description, it
 from pathlib import Path
 
-from crowd_anki.utils.filesystem.name_sanitizer import sanitize_anki_deck_name, invalid_filename_chars
+from crowd_anki.utils.filesystem.name_sanitizer import sanitize_anki_deck_name, \
+    invalid_filename_chars
 from test_utils.matchers import contain_any
 
 with description("AnkiDeckNameSanitizer"):
@@ -16,6 +17,7 @@ with description("AnkiDeckNameSanitizer"):
 
     with it("should be possible to create a file name from a random sanitized string"):
         @given(text(characters(min_codepoint=1, max_codepoint=800), max_size=255, min_size=1))
+        @example("line\n another one")
         def can_create(potential_name):
             assume(potential_name not in ('.', '..'))
             with TemporaryDirectory() as dir_name:
