915994d6f235b63b517c5788fc69f66a1e95e63a
Vladyslav Sitalo <sitalov@amazon.com>
2019-04-09 22:20:58 +0100

    Add notification on successful snapshot completion #45

--- crowd_anki/history/archiver_vendor.py
+++ crowd_anki/history/archiver_vendor.py
@@ -10,11 +10,13 @@ from ..anki.ui.utils import progress_indicator
 from ..export.anki_exporter import AnkiJsonExporter
 from ..utils.config import SNAPSHOT_ROOT_DECKS
 from ..utils.constants import USER_FILES_PATH
+from ..utils.notifier import Notifier, AnkiUiNotifier
 
 
 @dataclass
 class ArchiverVendor:
     window: Any
+    notifier: Notifier = field(default_factory=AnkiUiNotifier)
     config: dict = field(init=False)
 
     def __post_init__(self):
@@ -45,6 +47,8 @@ class ArchiverVendor:
         with progress_indicator(self.window, 'Taking CrowdAnki snapshot of all decks'):
             self.all_deck_archiver().archive(overrides=self.overrides(),
                                              reason=reason)
+            self.notifier.info("Snapshot successful",
+                               f"The CrowdAnki snapshot to {self.snapshot_path().resolve()} successfully completed")
 
     def overrides(self):
         return self.deck_manager.for_names(self.config.get(SNAPSHOT_ROOT_DECKS))
