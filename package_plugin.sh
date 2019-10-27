a05e5ab300884f730fd24e65f41821eed3ba83a8
Jordan Munch O'Hare <ohare93@gmail.com>
2019-10-27 18:34:14 +0100

    Pyuic5 build step

--- package_plugin.sh
+++ package_plugin.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pyuic5 ui_files/config.ui -o crowd_anki/config/config_ui.py
+
 ./fetch_dependencies.sh
 cd crowd_anki
 zip -r ../crowd_anki_$(date -u +"%Y%m%d").zip ./ -x "*__pycache__*" -x meta.json
