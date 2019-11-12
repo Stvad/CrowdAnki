7843ba68582c7da1312d081c0b741d57852d718f
Vladyslav Sitalo <sitalov@amazon.com>
2019-11-11 20:11:23 -0800

    Update the maximum allowed length for file name in sanitizer test

--- test/utils/filesystem/name_sanitizer_spec.py
+++ test/utils/filesystem/name_sanitizer_spec.py
@@ -16,7 +16,7 @@ with description("AnkiDeckNameSanitizer"):
             .not_to(contain_any(*invalid_filename_chars))
 
     with it("should be possible to create a file name from a random sanitized string"):
-        @given(text(characters(min_codepoint=1, max_codepoint=800), max_size=255, min_size=1))
+        @given(text(characters(min_codepoint=1, max_codepoint=800), max_size=254, min_size=1))
         @example("line\n another one")
         def can_create(potential_name):
             assume(potential_name not in ('.', '..'))
