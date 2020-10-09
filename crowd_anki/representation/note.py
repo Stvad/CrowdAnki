a2f6dd49ead0987cf5a81a27a69f5f576b54e144
Adam Plaice <plaice.adam+github@gmail.com>
2020-10-09 22:18:49 +0200

    Update note addition for Anki 2.1.28 and later
    col.addNote() is legacy.
    
    note.flush() is now impossible (and unnecessary) for a new note.

--- crowd_anki/representation/note.py
+++ crowd_anki/representation/note.py
@@ -104,11 +104,18 @@ class Note(JsonSerializableAnkiObject):
         # Todo uuid match on existing notes
 
         note_model = deck.metadata.models[self.note_model_uuid]
-        # You may ask WTF? Well, it seems anki has 2 ways to identify deck where to place card:
-        # 1) Looking for existing cards of this note
-        # 2) Looking at model->did and to add new cards to correct deck we need to modify the did each time
-        # ;(
-        note_model.anki_dict["did"] = deck.anki_dict["id"]
+
+        # Anki 2.1.28 made addition of notes slightly saner
+        # TODO: Refactor once compatibility with 2.1.26 is not needed.
+        # (2.1.27 doesn't seem to have existed.)
+        saner_note_addition = int(anki.version.split(".")[-1]) > 26
+
+        if not saner_note_addition:
+            # You may ask WTF? Well, it seems anki has 2 ways to identify deck where to place card:
+            # 1) Looking for existing cards of this note
+            # 2) Looking at model->did and to add new cards to correct deck we need to modify the did each time
+            # ;(
+            note_model.anki_dict["did"] = deck.anki_dict["id"]
 
         self.anki_object = UuidFetcher(collection).get_note(self.get_uuid())
         new_note = self.anki_object is None
@@ -122,12 +129,17 @@ class Note(JsonSerializableAnkiObject):
         self.anki_object.__dict__.update(self.anki_object_dict)
         self.anki_object.mid = note_model.anki_dict["id"]
         self.anki_object.mod = anki.utils.intTime()
-        self.anki_object.flush()
 
         if new_note:
-            collection.addNote(self.anki_object)
-        elif not import_config.ignore_deck_movement:
-            self.move_cards_to_deck(deck.anki_dict["id"])
+            if saner_note_addition:
+                collection.add_note(self.anki_object, deck.anki_dict["id"])
+            else:
+                self.anki_object.flush()
+                collection.addNote(self.anki_object)
+        else:
+            self.anki_object.flush()
+            if not import_config.ignore_deck_movement:
+                self.move_cards_to_deck(deck.anki_dict["id"])
 
     def handle_import_config_changes(self, import_config, note_model):
         # Personal Fields
