4306b59bb33a80c5ca96be41e7a5d2192a9a60c2
Adam Plaice <plaice.adam+github@gmail.com>
2021-12-11 14:25:44 +0100

    Mention limitations of hook approach for disambiguating deckconf

--- crowd_anki/utils/deckconf.py
+++ crowd_anki/utils/deckconf.py
@@ -1,3 +1,7 @@
+# Note that this only works for Anki ≤ 2.1.45, or when using the old
+# deck options popup with Anki 2.1.46+.  Anki 2.1.46+ has a new
+# JavaScript-based options popup (with no hooks), by default.
+
 from .constants import UUID_FIELD_NAME
 
 def disambiguate_crowdanki_uuid(deck_conf, deck,
