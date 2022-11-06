3308be32ece3cf0130e6567616d258d12c16f6ae
Adam Plaice <plaice.adam+github@gmail.com>
2022-11-06 22:36:33 +0100

    Avoid mocking Anki modules globally
    Ideally, we'd stop mocking them _at all_ but this is a good first
    step, as it's annoying and surprising (with the current/previous
    _global_ mocking, the test is/was run differently depending on whether
    it was run individually or as part of a batch).
    
    Fix #145.

--- test_utils/anki/__init__.py
+++ test_utils/anki/__init__.py
@@ -2,15 +2,26 @@ import sys
 
 from unittest.mock import MagicMock
 
-
-def mock_anki_modules():
+class MockAnkiModules:
     """
     I'd like to get rid of the situation when this is required, but for now this helps with the situation that
     anki modules are not available during test runtime.
     """
-
     modules_list = ['anki', 'anki.hooks', 'anki.exporting', 'anki.decks', 'anki.utils', 'anki.cards', 'anki.models',
-                    'anki.notes', 'aqt''', 'aqt.qt', 'aqt.exporting', 'aqt.utils']
+                    'anki.notes', 'aqt', 'aqt.qt', 'aqt.exporting', 'aqt.utils']
 
-    for module in modules_list:
-        sys.modules[module] = MagicMock()
+    def __init__(self):
+        self.shadowed_modules = {}
+
+        for module in self.modules_list:
+            self.shadowed_modules[module] = sys.modules.get(module)
+            sys.modules[module] = MagicMock()
+
+    def unmock(self):
+        for module in self.modules_list:
+            shadowed_module = self.shadowed_modules[module]
+            if shadowed_module is not None:
+                sys.modules[module] = shadowed_module
+            else:
+                if module in sys.modules:
+                    del sys.modules[module]
