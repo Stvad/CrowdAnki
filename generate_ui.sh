1db53155e8cbf527c75bfe86d9f665cfbc032931
Adam Plaice <plaice.adam+github@gmail.com>
2022-11-06 16:58:09 +0100

    Add generated ui.py files for qt6
    Fix #170.
    
    We're slavishly copying the convention used by anki in
    `qt/aqt/forms/` (up to the presence of the type: ignore comment).
    
    The old qt5 files are moved from *_ui.py to *_ui_qt5.py.
    
    The new qt6 files are *_ui_qt6.py.
    
    generate_ui.sh was run from within the pipenv (pipenv shell).  I'm not
    yet sure whether to change the script to have `pipenv run ...` at the
    start of all the lines, or to update CONTRIBUTING.md.

--- generate_ui.sh
+++ generate_ui.sh
@@ -2,5 +2,8 @@
 
 echo "Generating UI Files"
 
-pyuic5 ui_files/config.ui -o crowd_anki/config/config_ui.py
-pyuic5 ui_files/import.ui -o crowd_anki/importer/import_ui.py
+pyuic5 ui_files/config.ui -o crowd_anki/config/config_ui_qt5.py
+pyuic5 ui_files/import.ui -o crowd_anki/importer/import_ui_qt5.py
+
+pyuic6 ui_files/config.ui -o crowd_anki/config/config_ui_qt6.py
+pyuic6 ui_files/import.ui -o crowd_anki/importer/import_ui_qt6.py
