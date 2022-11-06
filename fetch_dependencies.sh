fa7702f45579486c99dcd34d9e58b00f441eb922
Adam Plaice <plaice.adam+github@gmail.com>
2022-11-06 17:21:59 +0100

    Update to latest pipenv and fix our tests
    `pipenv lock -r` has been replaced by `pipenv requirements`.
    
    I _think_ that the API is now slightly different, with the first line
    now describing the choice of index.  I don't think that this is an
    issue.

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pipenv run pip install --upgrade --no-binary :all: -r <(pipenv lock -r | sed -E "s/(^dulwich==.+$)/\1 --global-option=--pure/")  --target crowd_anki/dist
+pipenv run pip install --upgrade --no-binary :all: -r <(pipenv requirements | sed -E "s/(^dulwich==.+$)/\1 --global-option=--pure/")  --target crowd_anki/dist
