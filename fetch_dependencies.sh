06459ccd9b0ef69190624d7a1e4d84286b262a06
Adam Plaice <plaice.adam+github@gmail.com>
2023-05-17 23:36:08 +0200

    Update fetch_dependencies
    We need to replace --binary :all: with --binary $(pipenv requirements
    | sed -n 's/==.*//p' | tr '\n' ',') because otherwise pip tries to
    build everything from scratch (including the build system (??)).
    
    We need the new dulwich options:
    
    --config-settings "--global-option=--pure"
    
    because pip has changed the application of build options, so building
    pure python packages becomes harder.  See dulwich issue 1093.

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pipenv run pip install --upgrade --no-binary :all: -r <(pipenv requirements | sed -E "s/(^dulwich==.+$)/\1 --global-option=--pure/")  --target crowd_anki/dist
+pipenv run pip install --upgrade --no-binary $(pipenv requirements | sed -n 's/==.*//p' | tr '\n' ',') -r <(pipenv requirements | sed -E 's/(^dulwich==.+$)/\1 --config-settings "--global-option=--pure"/')  --target crowd_anki/dist
 
 # Check for Linux shared object files.  This won't work on Windows and might not work on MacOS.
 if [ ! "$(find crowd_anki/dist/ -name '*.so')" == "" ]
