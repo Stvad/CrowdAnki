d0e73b982a9d4b839ceaeab52834ba4eeb537b59
Adam Plaice <plaice.adam+github@gmail.com>
2023-05-23 12:45:08 +0200

    Avoid pipenv process substitution in fetch_dependencies.sh
    I don't fully understand why, but `pipenv run foo <(bar)` process
    substitution doesn't work in GitHub actions.
    
    AFAICT in a "normal" environment, pipenv execs into its subcommand
    process (here `foo`), and the subcommand inherits all its file
    descriptors (including the file descriptor for `<(bar)`, inherited
    from `fetch_dependencies.sh`.
    
    In GitHub actions, for unknown, to me, reasons, pipenv instead creates
    a separate child process for `foo` and does not share its file
    descriptors, so `foo` doesn't have access to `<(bar)`.

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+set -xe
 
-pipenv run pip install --upgrade --no-binary $(pipenv requirements | sed -n 's/==.*//p' | tr '\n' ',') -r <(pipenv requirements | sed -E 's/(^dulwich==.+$)/\1 --config-settings "--global-option=--pure"/')  --target crowd_anki/dist
+# Using `pipenv run foo <(bar)` process substitution doesn't work in GitHub actions, so we write to a temp file.
+pipenv requirements | sed -E 's/(^dulwich==.+$)/\1 --config-settings "--global-option=--pure"/' > tmp_requirements.txt
+pipenv run pip install --upgrade --no-binary "$(pipenv requirements | sed -n 's/==.*//p' | tr '\n' ',')" -r tmp_requirements.txt  --target crowd_anki/dist
+
+rm tmp_requirements.txt
 
 # Check for Linux shared object files.  This won't work on Windows and might not work on MacOS.
 if [ ! "$(find crowd_anki/dist/ -name '*.so')" == "" ]
