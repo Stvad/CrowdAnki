44fb8ebd44f07a1388eb024f821ce7677960d1fc
evandrocoan <evandrocoan@hotmail.com>
2024-07-24 12:20:40 -0300

    Set to remove old media directory before exporting it again to clean
    unused media.

--- crowd_anki/export/anki_exporter.py
+++ crowd_anki/export/anki_exporter.py
@@ -84,6 +84,7 @@ class AnkiJsonExporter(DeckExporter):
     def _copy_media(self, deck, deck_directory):
         media_directory = deck_directory.joinpath(MEDIA_SUBDIRECTORY_NAME)
 
+        shutil.rmtree(str(media_directory.resolve()), ignore_errors=True)
         media_directory.mkdir(parents=True, exist_ok=True)
 
         for file_src in deck.get_media_file_list():
